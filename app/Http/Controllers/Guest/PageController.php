dc2aa0f55c41281b432d00f234353ecb70ddea3a
AngeloRoudani <ange.rouda@gmail.com>
2023-05-12 16:41:29 +0200

    tentativo filtraggio treni

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index() {
-        $data = [
-            'ciao',
-            'bello'
-        ];
-        return view('home',compact('data'));
+        $current = date('Y-m-d');
+        $trains = Train::where('departed_hour','=','$current')->get();
+    
+
+        return view('home',compact('trains'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,25 @@
 
 @section('content')
 
-<div></div>
+<div class="row">
+    @foreach ($trains as $train )
+        <div class="card">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{$train->company}}</li>
+                <li class="list-group-item">{{$train->started_station}}</li>
+                <li class="list-group-item">{{$train->departed_hour}}</li>
+                <li class="list-group-item">{{$train->arrival_hour}}</li>
+                <li class="list-group-item">{{$train->code}}</li>
+                <li class="list-group-item">{{$train->wagons}}</li>
+                <li class="list-group-item">{{$train->delay}}</li>
+                <li class="list-group-item">{{$train->cancelled}}</li>
+            </ul>
+        </div>
+        
+    @endforeach
+    
+
+</div>
 
 
 @endsection
